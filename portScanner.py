3ad049684e5f2d6ca26bf3101015a10329f369a0
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-01-31 12:26:48 -0500

    v. 0.1
    - Added error handling to IP address and port limits
    - Fixed problems with while-loop implementation
    - Improved general code functionality and quality of output
    - Fixed issue with Windows-specific encoding
    - Tested script in Kali Linux virtual environment
    - Fixed problems with port number implementation

--- portScanner.py
+++ portScanner.py
@@ -3,33 +3,52 @@
 import socket
 import sys
 import subprocess
+import re
+import time
 from datetime import datetime
 
 # Code by Mackenzie Cleland, 2023
 
-subprocess.call('clear', shell=True)    # Clears terminal screen
-ipaddr = int(input("Please provide IP address of remote server to be scanned: "))  # Syntax handling?
+subprocess.call('clear', shell=True)    # Clears terminal
+
+while True:  # Retrieves IP address to be scanned
+    ipaddr = input("Please provide IP address of server to be scanned: ")
+    if not re.match(r'\d+(?:\.\d+){3}', ipaddr):  # Regex to match up IP syntax
+        print("Invalid IP address. Please try again.")
+    else:
+        break
+
 print("Starting port scanning operation...")    # Prompt
-# wait()
+time.sleep(3)
 
 
-def scanHost():     # Reads host_config.txt and uses its information to scan the host system
+def scanHost():     # Establishes upper and lower limits on ports, then scans IP using this range.
     timea = datetime.now()
 
-    limit = int(input("Please provide the highest port number you wish to scan: ")) + 1  # TypeError handling?
     while True:
-        if limit > 65535 or limit < 1:
+        ulim = int(input("Please provide the highest port number you wish to scan: ")) + 1
+        if ulim > 65535 or ulim < 1:
+            print("Invalid port number. Please try again.")
+        else:
+            break
+
+    while True:
+        olim = int(input("Please provide the lowest port number you wish to scan: "))  # TypeError handling?
+        if olim > 65535 or olim < 1 or olim > ulim:
             print("Invalid port number. Please try again.")
             sys.exit()
         else:
             break
 
+    print(f"Scanning ports {olim} - {ulim}...")
     try:
-        for p in range(1, limit):
-            csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cconnect = csocket.connect_ex((ipaddr, p))
-            if cconnect == 0:
+        for p in range(olim, ulim):
+            csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # This syntax may be Unix-exclusive (?)
+            cconn = csocket.connect_ex((ipaddr, p))
+            if cconn == 0:
                 print(f"Port {p} is open!")
+            else:
+                print(f"Port {p} is closed!")
             csocket.close()
     except KeyboardInterrupt:
         print("Keyboard interrupt detected. Exiting program.")
@@ -43,15 +62,13 @@ def scanHost():     # Reads host_config.txt and uses its information to scan the
 
     timeb = datetime.now()
     total = timeb - timea
-    print(f"Scan complete! Total time elapsed: {total}")
+    print(f"Completed scan of ports {olim} - {ulim} on server with IP address {ipaddr}")
+    print(f"Total time elapsed: {total}")
 
 
 scanHost()
 
 # TO DO:
-# Implement error handling for IP address and port number upper limit
 # Implement config file generation
 # Implement reading from config file
 # Implement better error handling for scanning than just exiting, if possible
-# Implement lower limit for port scanning range
-
