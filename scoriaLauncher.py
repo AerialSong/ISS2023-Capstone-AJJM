5ba3d7f404c4b22c086aa09b443edea525361adb
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-02-02 18:08:36 -0700

    Add files via upload
    This is intended to serve as the main launcher, meaning that this is what the terminal will launch upon the user entering the required argument.

---
+++ scoriaLauncher.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Code by Mackenzie Cleland, 2023
+
+def printBanner():
+    with open("scoriaBanner.txt", "r") as f:
+        f.readlines()
+        f.close()
+
+
+printBanner()
