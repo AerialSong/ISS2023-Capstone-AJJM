11c0b97039c4a57b79cb533b9a41b1a8b2036edf
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-02-09 13:16:34 -0700

    Update launcher.py
    - Changed filename
    - Changed .txt name to reflect new filename
    - Added flow control for whole program

--- launcher.py
+++ launcher.py
@@ -2,11 +2,34 @@
 
 # Code by Mackenzie Cleland, 2023
 
-def printBanner():
+# Implement bash syntax to launch Python3 first, then run the rest inside the Python shell, likely via venv module or similar environment.
+
+
+def printBanner():  # Prints the main banner for the program
     with open("banner.txt", "r") as f:
         print(f.read())
 
 # This code will likely expand to include calls for launching each of our scripts; it will thus be the main file called by the terminal.
+
+
+def runScripts():  # Flow control for main program functionality
+    while True:
+        print("Welcome to SCORIA! What would you like to do?")
+        print("")
+        choice = int(input("1. Scan ports\n2. Monitor traffic\n3. Export logs to cloud"))
+
+        if choice == 1:
+            # run portScanner.py
+            break
+        elif choice == 2:
+            # run netMonitor.py
+            break
+        elif choice == 3:
+            # run cloud script
+            break
+        else:
+            print("Invalid choice detected! Please try again.")
+            print("")
 
 
 printBanner()
