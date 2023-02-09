f2123f50463140af8cd024845051dd3fa9824801
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-02-09 13:14:59 -0700

    Update and rename scoriaLauncher.py to launcher.py
    - Changed filename
    - Changed name of .txt file to reflect new filename

--- launcher.py
+++ launcher.py
@@ -3,7 +3,7 @@
 # Code by Mackenzie Cleland, 2023
 
 def printBanner():
-    with open("scoriaBanner.txt", "r") as f:
+    with open("banner.txt", "r") as f:
         print(f.read())
 
 # This code will likely expand to include calls for launching each of our scripts; it will thus be the main file called by the terminal.
