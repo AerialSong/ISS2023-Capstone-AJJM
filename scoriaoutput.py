545314b324f3d2592ca9b5171da32b0f50fbdafa
AerialSong <93795686+AerialSong@users.noreply.github.com>
2023-02-13 16:44:44 -0700

    Added srcport, destport, srcmac and destmac filter options

--- scoriaoutput.py
+++ scoriaoutput.py
@@ -34,8 +34,14 @@ def printargs(args):
                     args.source = mydict["source"]
                 if mydict["protocol"] != None:
                     args.protocol = mydict["protocol"]
-                if mydict["port"] != None:
-                    args.port = mydict["port"]
+                if mydict["srcport"] != None:
+                    args.srcport = mydict["srcport"]
+                if mydict["destport"] != None:
+                    args.destport = mydict["destport"]
+                if mydict["srcmac"] != None:
+                    args.srcmac = mydict["srcmac"]
+                if mydict["destmac"] != None:
+                    args.destmac = mydict["destmac"]
                 if mydict["date"] != None:
                     args.date = mydict["date"]
                 if mydict["time"] != None:
@@ -70,11 +76,29 @@ def printargs(args):
             elif args.protocol != None and args.protocol[0] == 'none':
                 args.protocol = None
 
-            if args.port != None and args.port[0] != 'none':
+            if args.srcport != None and args.srcport[0] != 'none':
                 print("|", end='')
-                print(f" Port: {args.port[0]} ", end='')
-            elif args.port != None and args.port[0] == 'none':
-                args.port = None
+                print(f" Source Port: {args.srcport[0]} ", end='')
+            elif args.srcport != None and args.srcport[0] == 'none':
+                args.srcport = None
+
+            if args.destport != None and args.destport[0] != 'none':
+                print("|", end='')
+                print(f" Destination Port: {args.destport[0]} ", end='')
+            elif args.destport != None and args.destport[0] == 'none':
+                args.destport = None
+
+            if args.srcmac != None and args.srcmac[0] != 'none':
+                print("|", end='')
+                print(f" Source MAC: {args.srcmac[0]} ", end='')
+            elif args.srcmac != None and args.srcmac[0] == 'none':
+                args.srcmac = None
+
+            if args.destmac != None and args.destmac[0] != 'none':
+                print("|", end='')
+                print(f" Destination MAC: {args.destmac[0]} ", end='')
+            elif args.destmac != None and args.destmac[0] == 'none':
+                args.destmac = None
 
             if args.date != None and args.date[0] != 'none':
                 print("|", end='')
@@ -103,7 +127,10 @@ if __name__ == '__main__':
     parser.add_argument("-dest", "--destination", type=str, nargs=1, metavar="destination_ip", default=None, help="Destination IP Address")
     parser.add_argument("-s", "--source", type=str, nargs=1, metavar="source_ip", default=None, help="Source IP Address")
     parser.add_argument("-pr", "--protocol", type=str, nargs=1, metavar="protocol_name", default=None, help="Type of protocol")
-    parser.add_argument("-p", "--port", type=str, nargs=1, metavar="port_num", default=None, help="Port Number")
+    parser.add_argument("-sp", "--srcport", type=str, nargs=1, metavar="src_port_num", default=None, help="Source Port Number")
+    parser.add_argument("-dp", "--destport", type=str, nargs=1, metavar="dest_port_num", default=None, help="Destination Port Number")
+    parser.add_argument("-sm" , "--srcmac", type=str, nargs=1, metavar="src_mac", default=None, help="Source MAC address")
+    parser.add_argument("-dm" , "--destmac", type=str, nargs=1, metavar="dest_mac", default=None, help="Destination MAC address")
     parser.add_argument("-d", "--date", type=str, nargs=1, metavar="date", default=None, help="Date packet was made; Syntax = MMdd")
     parser.add_argument("-t", "--time", type=str, nargs=1, metavar="time", default=None, help="Time packet was made; Syntax = HHmm")
 
