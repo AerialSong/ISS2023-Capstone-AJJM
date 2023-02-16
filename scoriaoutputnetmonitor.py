98c382608f404d5a9a2b5c07cff82a4e371cf20b
AerialSong <93795686+AerialSong@users.noreply.github.com>
2023-02-16 14:43:13 -0700

    Added full filtration functionality with some syntax catching

--- scoriaoutputnetmonitor.py
+++ scoriaoutputnetmonitor.py
@@ -2,13 +2,15 @@ import argparse
 import json
 import os
 from os.path import exists
-import sys
 import time as clock
 import socket
 import struct
 import textwrap
 import datetime
 import subprocess
+import re
+import sys
+import ipaddress
 
 # Code by Arthur Kutepov, Jomel Jay 2023
 
@@ -30,9 +32,6 @@ def monitor(args):
     format_star2 = "\t*"
     format_tab = "\t   "
     path = "arg.txt"
-    arglist = []
-    arglist.append(args.destination, args.source, args.srcport, args.destport, args.srcmac, args.destmac, args.date, args.time)
-
     
     # local list variable to store json namespace data
     mydict = {}
@@ -70,6 +69,55 @@ def monitor(args):
             print("\nExiting Program...\n")
             sys.tracebacklimit = 0'''
 
+        # If entered argument is "none" then the argument becomes null
+        if args.destination != None and args.destination[0] == 'none':
+            args.destination = None
+
+        if args.source != None and args.source[0] == 'none':
+            args.source = None
+
+        if args.protocol != None and args.protocol[0] == 'none':
+            args.protocol = None
+
+        if args.srcport != None and args.srcport[0] == 'none':
+            args.srcport = None
+
+        if args.destport != None and args.destport[0] == 'none':
+            args.destport = None
+
+        if args.srcmac != None and args.srcmac[0] == 'none':
+            args.srcmac = None
+            
+        if args.destmac != None and args.destmac[0] == 'none':
+            args.destmac = None
+
+        if args.date != None and args.date[0] == 'none':
+            args.date = None
+
+        if args.time != None and args.time[0] == 'none':
+            args.time = None
+
+        arglist = []
+
+        if args.destination != None:
+            arglist.append(args.destination[0])
+        if args.source != None:
+            arglist.append(args.source[0])
+        if args.protocol != None:
+            arglist.append(args.protocol[0])
+        if args.srcport != None:
+            arglist.append(args.srcport[0])
+        if args.destport != None:
+            arglist.append(args.destport[0])
+        if args.srcmac != None:
+            arglist.append(args.srcmac[0])
+        if args.destmac != None:
+            arglist.append(args.destmac[0])
+        if args.date != None:
+            arglist.append(args.date[0])
+        if args.time != None:
+            arglist.append(args.time[0])
+
 
     # the plan: upon entering an argument, say if someone searches for -dest 3.3.3.3, then it will check if 
     # the destination ip matches that
@@ -86,63 +134,67 @@ def monitor(args):
     # Checks if ANY of the argument values or NOT NONE
     # If ANY of them are NOT NONE, then it can do the filter functionality
 
-
-    # A loop to contantly receive a packet
+        # Packets are constantly received
+        packetlist = []
         inet_data, addr = conn.recvfrom(65536) # buffersize
         dst_mac, src_mac, L2_proto, data = L2_frame(inet_data)
-        print(f"| Num: {packet_num} | Src MAC: {src_mac} | Dest MAC: {dst_mac} ", end='')
-        #print("-" * 200, f"\nNumber {packet_num} | Date and time: {datetime_now}")
-        #print(format_star1, f"Ethernet Frame {packet_num} - Dst MAC: {dst_mac}, Src MAC: {src_mac}, Ethernet Protocol ID: {L2_proto}")
-        packet_num += 1
-   
+        packetlist.append(dst_mac)
+        packetlist.append(src_mac)
+        proto = ""
         # Ethernet frame ID 8 is IPv4
-        if L2_proto == 8 and args.destination == None and args.source == None:
+        if L2_proto == 8:
             (time_to_live, L3_proto, src_IP, dst_IP, data) = L3_packet(data)
-            print(f"L3_proto: {L3_proto}")
-            print(f"| Dest IP: {dst_IP} | Source IP: {src_IP} ", end='')
-            #print(format_star1, f"IPv4 Packet - Src IP: {src_IP}, Dst IP: {dst_IP}, IP Protocol ID: {L3_proto}, TTL: {time_to_live}")
-
-        elif L2_proto == 8 and args.destination != None:
-            (time_to_live, L3_proto, src_IP, dst_IP, data) = L3_packet(data)
-            if dst_IP == args.destination[0]:
-                print(f"| Dest IP: {dst_IP} ", end='')
-        
-        elif L2_proto == 8 and args.source != None:
-            (L3_proto, src_IP, data) = L3_packet(data)
-            if src_IP == args.source[0]:
-                print(f"| Source IP: {src_IP} ", end='')
-
+            packet_num += 1
+            packetlist.append(src_IP)
+            packetlist.append(dst_IP)
         # IP Protocol ID 1 is ICMP
         if L3_proto == 1:
             icmp_type, checksum, data = icmp_unpack(data)
-            #print(format_star1, f"ICMP Packet - Type: {icmp_type}, Checksum: {checksum}")
-            #print(format_star2, "Data:\n", line_format(format_tab, data))
-         
-        # IP Protocol ID 6 is TCP
+        # IP protocol ID 6 is TCP
         elif L3_proto == 6:
             (src_port, dst_port, seq, ack, data) = tcp_unpack(data)
-            print(f"| Protocol: TCP | Src Port: {src_port} | Dest Port: {dst_port} |\n", end='')
-            #print(format_star1, f"TCP Segment - Src Port: {src_port}, Dst Port: {dst_port}, Seq: {seq}, Ack: {ack}")
-            #print(format_star2, "Data:\n", line_format(format_tab, data))
-         
-        # IP Protocol ID 17 is UDP
+            proto = "TCP"
+            packetlist.append(src_port)
+            packetlist.append(dst_port)
+            packetlist.append(proto)
+        # IP protocol ID 17 is UDP
         elif L3_proto == 17:
             src_port, dst_port, size, data = udp_unpack(data)
-            print(f"| Protocol: UDP | Src Port: {src_port} | Dest Port: {dst_port} |\n")
-            #print(format_star1, f"UDP Segment - Src Port: {src_port}, Dst Port: {dst_port}, Size: {size}")
-            #print(format_star2, "Data:\n", line_format(format_tab, data))
+            proto = "UDP"
+            packetlist.append(src_port)
+            packetlist.append(dst_port)
+            packetlist.append(proto)
+        
+        #print(arglist)
+        #print(packetlist)
 
-        # Other IP Protocols
-        '''
-        else:
-        print(format_star1, "Data:")
-            print(line_format(format_tab, data))'''
-      
-        '''
-        else:
-            print("Data:")
-            print(line_format(format_tab, data))'''
-        clock.sleep
+
+        # If every argument is Null, then packets print normally
+        if args.destination == None and args.source == None and args.protocol == None and args.srcport == None and args.destport == None and args.srcmac == None and args.destmac == None and args.date == None and args.time == None:
+            print(f"| Num: {packet_num} | Src MAC: {src_mac} | Dest MAC: {dst_mac} ", end='')
+   
+            # Ethernet frame ID 8 is IPv4
+            if L2_proto == 8:
+                print(f"| Dest IP: {dst_IP} | Source IP: {src_IP} ", end='')
+
+            # IP Protocol ID 1 is ICMP
+            if L3_proto == 1:
+                icmp_type, checksum, data = icmp_unpack(data)
+         
+            print(f"| Protocol: {proto} | Src Port: {src_port} | Dest Port: {dst_port} |\n", end='')
+
+        elif set(arglist).issubset(packetlist) == True:
+            print(f"| Num: {packet_num} | Src MAC: {src_mac} | Dest MAC: {dst_mac} ", end='')
+            
+            # Ethernet frame ID 8 is IPv4
+            if L2_proto == 8:
+                print(f"| Dest IP: {dst_IP} | Source IP: {src_IP} ", end='')
+
+            # IP Protocol ID 1 is ICMP
+            if L3_proto == 1:
+                icmp_type, checksum, data = icmp_unpack(data)
+         
+            print(f"| Protocol: {proto} | Src Port: {src_port} | Dest Port: {dst_port} |\n", end='')
       
 
 
@@ -205,124 +257,48 @@ def line_format(prefix, string, size=80):
 
 
 # Print function. All arguments are sent here and printed
-'''def printargs(args):
-    path = "arg.txt"
-    
-    # local list variable to store json namespace data
-    mydict = {}
-    try:
-        while True:
-            # Checks if newly created txt file exists
-            # if so, the arguments are then changed in real time
-            if exists(path):
-                with open(path, "r") as f:
-                    mydict = json.load(f)
-                    f.close()
-                os.remove(path)
-                   
-
-                if mydict["destination"] != None:
-                    args.destination = mydict["destination"]
-                if mydict["source"] != None:
-                    args.source = mydict["source"]
-                if mydict["protocol"] != None:
-                    args.protocol = mydict["protocol"]
-                if mydict["srcport"] != None:
-                    args.srcport = mydict["srcport"]
-                if mydict["destport"] != None:
-                    args.destport = mydict["destport"]
-                if mydict["srcmac"] != None:
-                    args.srcmac = mydict["srcmac"]
-                if mydict["destmac"] != None:
-                    args.destmac = mydict["destmac"]
-                if mydict["date"] != None:
-                    args.date = mydict["date"]
-                if mydict["time"] != None:
-                    args.time = mydict["time"]
-        
-                
-        # Checks if each argument in the namespace is null or not
-        # If not, they print
-
-        # Additional if statements are here to check if the entered string of an argument is none
-        # If so, it will define the entered argument as None
-            if args.destination != None and args.destination[0] != 'none':
-                print("|", end='')
-                print(f" Dest IP: {args.destination[0]} ", end='')
-            elif args.destination != None and args.destination[0] == 'none':
-                args.destination = None
-
-            if args.source != None and args.source[0] != 'none':
-                print("|", end='')
-                print(f" Source IP: {args.source[0]} ", end='')
-            elif args.source != None and args.source[0] == 'none':
-                args.source = None
-
-            if args.protocol != None and args.protocol[0] != 'none':
-                print("|", end='')
-                print(f" Protocol: {args.protocol[0]} ", end='')
-            elif args.protocol != None and args.protocol[0] == 'none':
-                args.protocol = None
-
-            if args.srcport != None and args.srcport[0] != 'none':
-                print("|", end='')
-                print(f" Source Port: {args.srcport[0]} ", end='')
-            elif args.srcport != None and args.srcport[0] == 'none':
-                args.srcport = None
-
-            if args.destport != None and args.destport[0] != 'none':
-                print("|", end='')
-                print(f" Destination Port: {args.destport[0]} ", end='')
-            elif args.destport != None and args.destport[0] == 'none':
-                args.destport = None
-
-            if args.srcmac != None and args.srcmac[0] != 'none':
-                print("|", end='')
-                print(f" Source MAC: {args.srcmac[0]} ", end='')
-            elif args.srcmac != None and args.srcmac[0] == 'none':
-                args.srcmac = None
-            
-            if args.destmac != None and args.destmac[0] != 'none':
-                print("|", end='')
-                print(f" Destination MAC: {args.destmac[0]} ", end='')
-            elif args.destmac != None and args.destmac[0] == 'none':
-                args.destmac = None
-
-            if args.date != None and args.date[0] != 'none':
-                print("|", end='')
-                print(f" Date: {args.date[0]} ", end='')
-            elif args.date != None and args.date[0] == 'none':
-                args.date = None
-
-            if args.time != None and args.time[0] != 'none':
-                print("|", end='')
-                print(f" Time: {args.time[0]} ", end='')
-            elif args.time != None and args.time[0] == 'none':
-                args.time = None
-
-            print("|", end='')
-            clock.sleep(1)
-            print("\n")
-        
-    except KeyboardInterrupt:
-        print("\nExiting Program...\n")
-        sys.tracebacklimit = 0'''
 
 if __name__ == '__main__':
     # parser object
     parser = argparse.ArgumentParser(description="A lightweight command-line based Network Traffic Analyzer")
 
-    parser.add_argument("-dest", "--destination", type=str, nargs=1, metavar="destination_ip", default=None, help="Destination IP Address")
-    parser.add_argument("-s", "--source", type=str, nargs=1, metavar="source_ip", default=None, help="Source IP Address")
-    parser.add_argument("-pr", "--protocol", type=str, nargs=1, metavar="protocol_name", default=None, help="Type of protocol")
-    parser.add_argument("-sp", "--srcport", type=str, nargs=1, metavar="src_port_num", default=None, help="Source Port Number")
-    parser.add_argument("-dp", "--destport", type=str, nargs=1, metavar="dest_port_num", default=None, help="Destination Port Number")
-    parser.add_argument("-sm" , "--srcmac", type=str, nargs=1, metavar="src_mac", default=None, help="Source MAC address")
-    parser.add_argument("-dm" , "--destmac", type=str, nargs=1, metavar="dest_mac", default=None, help="Destination MAC address")
-    parser.add_argument("-d", "--date", type=str, nargs=1, metavar="date", default=None, help="Date packet was made; Syntax = MMdd")
-    parser.add_argument("-t", "--time", type=str, nargs=1, metavar="time", default=None, help="Time packet was made; Syntax = HHmm")
+    parser.add_argument("-dest", "--destination", type=str, nargs=1, metavar="destination_ip", default=None, help="Specify your desired Destination IP Address for filtration; Syntax = 111.222.333.444")
+    parser.add_argument("-s", "--source", type=str, nargs=1, metavar="source_ip", default=None, help="Specify your desired source IP Address for filtration; Syntax = 111.222.333.444")
+    parser.add_argument("-pr", "--protocol", type=str, nargs=1, metavar="protocol_name", default=None, help="Specify your desired protocol name for filtration; TCP or UDP")
+    parser.add_argument("-sp", "--srcport", type=str, nargs=1, metavar="src_port_num", default=None, help="Specify your desired Source Port number for filtration")
+    parser.add_argument("-dp", "--destport", type=str, nargs=1, metavar="dest_port_num", default=None, help="Specify your desired Destination Port number for filtration")
+    parser.add_argument("-sm" , "--srcmac", type=str, nargs=1, metavar="src_mac", default=None, help="Specify your desired Source Mac address for filtration; Syntax = 00:00:00:00:00:00")
+    parser.add_argument("-dm" , "--destmac", type=str, nargs=1, metavar="dest_mac", default=None, help="Specify your desired Destination Mac address for filtration; Syntax = 00:00:00:00:00:00")
+    parser.add_argument("-d", "--date", type=str, nargs=1, metavar="date", default=None, help="Specify your desired date of packet creation for filtration; Syntax = MMdd")
+    parser.add_argument("-t", "--time", type=str, nargs=1, metavar="time", default=None, help="Specify your desired time of packet creation for filtration; Syntax = HHmm")
 
     args = parser.parse_args()
+
+    if args.destination != None:
+        try:
+            ipaddress.IPv4Address(args.destination[0])
+        except ipaddress.AddressValueError:  
+            sys.exit("Incorrect syntax for Destination IP! Reenter the option with this syntax: 123.123.123.123\nExiting program...")   
+    else:
+        pass
+
+    if args.source != None:
+        try:
+            ipaddress.IPv4Address(args.source[0])
+        except ipaddress.AddressValueError:  
+            sys.exit("Incorrect syntax for Source IP! Reenter the option with this syntax: 123.123.123.123\nExiting program...")   
+    else:
+        pass
+
+    if args.srcport != None:
+        try:
+            if int(args.srcport[0]) not in range(1, 65535):
+                sys.exit("Entered Source port argument not found in range 1-65535! Reenter the option within that range\nExiting Program...")
+        except ValueError:
+            sys.exit("Entered Source port option is not an integer! Reenter as an integer within the range 1-65535\nExiting Program...")
+    else:
+        pass
+
 
     # Executing the Program
     try: 
