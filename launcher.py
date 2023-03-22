7290459d5097355f535777ae25c2a6c950ddb2aa
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-03-22 17:00:52 -0600

    v.1.3
    CHANGELOG:
    -Fixed issue with scripts not relaunching correctly
    -Added netmon and sigids scripts to launcher

--- launcher.py
+++ launcher.py
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
 
 import sys
+import importlib
 
 # Code by Mackenzie Cleland, 2023
-
-# Implement bash syntax to launch Python3 first, then run the rest inside the Python shell, likely via venv module or similar environment.
 
 
 def printBanner():  # Prints the main banner for the program
@@ -32,18 +31,42 @@ def runScripts():  # Flow control for main program functionality
     while True:
         print("Welcome to SCORIA! What would you like to do?")
         print("")
-        choice = int(input("1. Scan ports\n2. Monitor traffic\n3. Export logs to cloud\n4. Quit program\n"))
+        choice = int(input("1. Scan ports\n2. Monitor traffic\n3. Detect intrusions on host\n4. Export logs to cloud\n5. Quit program\n"))
 
         if choice == 1:
-            import ports  # Can't find functions in script!
-            launchFlow()
+            if ports not in sys.modules:
+                import ports
+            else:
+                importlib.reload(ports)
+            launchFlow()  # Not sure if should be indented
         elif choice == 2:
-            # import scoriaoutputnetmonitor
+            if sniffer not in sys.modules:
+                import sniffer
+            else:
+                importlib.reload(sniffer)
+            if linput not in sys.modules:
+                import linput
+            else:
+                importlib.reload(linput)
             launchFlow()
         elif choice == 3:
-            print("Cloud functionality coming soon! :D")
+            if sigids not in sys.modules:
+                import sigids
+            else:
+                importlib.reload(sigids)
+            if netmon not in sys.modules:
+                import netmon
+            else:
+                importlib.reload(netmon)
             launchFlow()
         elif choice == 4:
+            print("Cloud functionality coming soon! :D")
+            if s3nder not in sys.modules:
+                import s3nder
+            else:
+                importlib.reload(s3nder)
+            launchFlow()
+        elif choice == 5:
             print("Acknowledged. Exiting program...")
             sys.exit()
         else:
@@ -54,6 +77,3 @@ def runScripts():  # Flow control for main program functionality
 printBanner()
 runScripts()
 
-
-# TO DO:
-# Implement import statements for our other scripts!
