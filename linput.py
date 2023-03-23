d57422d54648fdc394f2d6787a097a95f3f40d68
AerialSong <93795686+AerialSong@users.noreply.github.com>
2023-03-23 14:18:48 -0600

    Added shebang and another argparse option for sleep

--- linput.py
+++ linput.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import argparse
 import json
 
@@ -15,6 +16,7 @@ def main():
     parser.add_argument("-d", "--date", type=str, nargs=1, metavar="date", default=None, help="Specify your desired date of packet creation for filtration; Syntax = MMdd")
     parser.add_argument("-t", "--time", type=str, nargs=1, metavar="time", default=None, help="Specify your desired time of packet creation for filtration; Syntax = HHmm")
     parser.add_argument("-c", "--clear", action="store_true", help="Clear all already entered arguments")
+    parser.add_argument("-sl", "--sleep", type=float, nargs=1, metavar="sleep_sec", default=None, help="Specify how many seconds you would like the output to sleep upon printing a packet; for an easier to follow output.")
 
     args = parser.parse_args()
 
