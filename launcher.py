074c2d13fbcd282632b65be5d3ee51e4a07821bd
MClelISS <102828998+MClelISS@users.noreply.github.com>
2023-03-23 13:55:57 -0600

    launcher.py
    v. 1.4
    CHANGELOG:
    -Overhauled imports of custom modules
    -Fixed some issues with scripts not needing to be imported
    -Added explanation of sniffer.py only working on Linux systems

---
+++ launcher.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import sys
+
+import ports
+import netIDS
+# import sniffer
+
+# Code by Mackenzie Cleland, 2023
+
+
+def printBanner():  # Prints the main banner for the program
+    with open("banner.txt", "r") as f:  # FileNotFoundError
+        print(f.read())
+        f.close()
+
+
+# def launchFlow():  # Flow control for individual script execution
+#         while True:
+#             yesnt = input("Do you wish to run this service again? Y/N\n")
+#             if yesnt == "Y" or yesnt == "y":
+#                 print("Running service again.\n")
+#                 runScripts()
+#                 print("")
+#             elif yesnt == "N" or yesnt == "n":
+#                 print("Acknowledged. Exiting program.")
+#                 sys.exit()
+#             else:
+#                 print("Invalid input detected! Please try again.\n")
+
+
+def runScripts():  # Flow control for main program functionality
+
+    while True:
+        print("Welcome to SCORIA! What would you like to do?")
+        print("")
+        choice = int(input("1. Scan ports\n2. Monitor traffic (Currently only works on Linux systems!!!)\n3. Detect intrusions on host\n4. Quit program\n"))
+
+        if choice == 1:
+            ports.main()
+            print("\n")
+        # elif choice == 2:
+        #     sniffer.main()
+        #     launchFlow()
+        elif choice == 3:
+            netIDS.main()
+            print("\n")
+        #     launchFlow()
+        elif choice == 4:
+            print("Acknowledged. Exiting program...")
+            sys.exit()
+        else:
+            print("Invalid choice detected! Please try again.")
+            print("")
+
+
+printBanner()
+runScripts()
